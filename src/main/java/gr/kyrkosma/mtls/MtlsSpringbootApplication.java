9abc8e8a3975062179d773e3814bf508b3b15b42
kyrkosma <kyrkosma@gmail.com>
2024-12-05 17:05:29 +0100

    Initial commit

---
+++ src/main/java/gr/kyrkosma/mtls/MtlsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package gr.kyrkosma.mtls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtlsSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtlsSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gr/kyrkosma/mtls/MtlsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.kyrkosma.mtls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtlsSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
